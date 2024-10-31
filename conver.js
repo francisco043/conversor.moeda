97a66cdda3d5fcfe666635a4d3f7eb40ee5cf178
Your Name <you@example.com>
2024-10-31 07:27:51 -0300

    mudança valor no java

--- conver.js
+++ conver.js
@@ -10,8 +10,8 @@ document.getElementById ('currency-converter').addEventListener('submit', functi
     const exchangeRates = {
         USD: { BRL: 5.70, EUR: 0.93 },
         BRL: { USD: 0.18, EUR: 0.16 },
-        EUR: { USD: 1.08, EUR: 6.16 },
-        EUR: { USD: 1.08, EUR: 6.16 }
+        EUR: { USD: 1.08, BRL: 6.16 },
+   
     };
  
     // Conversão simples de moeda
